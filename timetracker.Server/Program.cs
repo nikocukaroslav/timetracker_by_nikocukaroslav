078cce128bc41fcb3b5d689fd4c72e33e7abc2fd
Yaroslav Nikonchuk <ipz234_nyaf@student.ztu.edu.ua>
2024-07-18 22:39:51 +0300

    project setup

---
+++ timetracker.Server/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();

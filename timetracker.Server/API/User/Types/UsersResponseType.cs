ec84931fcc7f67093fa359f613b78941464f1f14
Волинець Андрій Сергійович <ipz234_vas@student.ztu.edu.ua>
2024-07-27 20:59:16 +0300

    [FEATURE] delete user mutation
    - add DeleteUser mutation
    - add field "Email" to UsersResponceType

--- timetracker.Server/API/User/Types/UsersResponseType.cs
+++ timetracker.Server/API/User/Types/UsersResponseType.cs
@@ -12,6 +12,7 @@ namespace timetracker.Server.API.User.Types
             Field(t => t.Id);
             Field(t => t.Name);
             Field(t => t.Surname);
+            Field(t => t.Email);
             Field(t => t.EmployeeType);
             Field<ListGraphType<StringGraphType>>("permissions")
                 .AuthorizeWithPolicy(Permissions.MANAGE_USERS.ToString())
--- timetracker.Server/API/User/UserMutation.cs
+++ timetracker.Server/API/User/UserMutation.cs
@@ -22,7 +22,7 @@ namespace timetracker.Server.API.User
 
                     Entities.User user = context.GetArgument<Entities.User>("user");
 
-                    if (user == null)
+                    if (user is null)
                     {
                         context.Errors.Add(new ExecutionError("Invalid credentials")
                         {
@@ -30,9 +30,30 @@ namespace timetracker.Server.API.User
                         });
                         return null;
                     }
+
                     user.Password = passwordHasher.HashPassword(user.Password);
 
                     return await userRepository.AddAsync(user);
+                });
+            Field<StringGraphType>("DeleteUser")
+                .AuthorizeWithPolicy(Permissions.MANAGE_USERS.ToString())
+                .Arguments(new QueryArguments(new QueryArgument<GuidGraphType> { Name = "id" }))
+                .ResolveAsync(async context =>
+                {
+                    Guid id = context.GetArgument<Guid>("id");
+                    var user = await userRepository.GetByIdAsync(id);
+                    if (user is null)
+                    {
+                        context.Errors.Add(new ExecutionError("User is not found")
+                        {
+                            Code = ExceptionsCode.USER_NOT_FOUND.ToString(),
+                        });
+                        return null;
+                    }
+
+                    await userRepository.DeleteAsync(id);
+
+                    return "User deleted successful";
                 });
         }
     }
